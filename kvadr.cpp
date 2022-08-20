a13b17f971b0e58250ae64cab651c8851e9bb133
gr1nd3 <111478576+gr1nd3@users.noreply.github.com>
2022-08-20 19:29:15 +1000

    Add files via upload

--- kvadr.cpp
+++ kvadr.cpp
@@ -6,20 +6,21 @@
 setlocale (LC_ALL, "Rus");
 
 const double DISK = 0.0001;
-
+char ch;
 double a = 0, b = 0, c = 0, d = 0, x1 = 0, x2 = 0;
-int prod = 1, chikl = 0, pustm = 0;
+int pustm = 0;
 
 printf("Введите коэффициенты квадратного уравнения. \n");
-chikl = scanf("%lf %lf %lf", &a, &b, &c);
 
-while (chikl != 3)
+
+
+while (scanf("%lf %lf %lf", &a, &b, &c) != 3)
     {
-    printf("Введенные данные некоректны. Повторите ввод. \n");
-    fflush(stdin);
-    chikl = scanf("%lf %lf %lf", &a, &b, &c);
-    }
+while (getchar() != '\n'){}
 
+    printf("Введенные данные некоректны. Повторите ввод. \n");
+
+    }
  //тут данные должны как-то отправиться в другой файл, на выходе которого мы получим x1, x2 и pustm - переменную, определяющую количество корней
 
 d = b * b - 4 * a * c;                     // это кусок, обрабатываемый этим самым файлом
